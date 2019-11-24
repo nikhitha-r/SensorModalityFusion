695b8bc102c547e63e92940df4393dc0583f6f7f
hcg <chenguang.huang@siemens.com>
2019-11-24 23:02:52 +0100

    add dataloader for pointfusion

--- pointfusion/Data Processing_remote.py
+++ pointfusion/Data Processing_remote.py
@@ -4,12 +4,11 @@
 # In[ ]:
 
 
-
-
-
 # In[2]:
 
 
+from keras.backend.tensorflow_backend import set_session
+from keras.applications import resnet50
 import numpy as np
 import sys
 import os
@@ -38,7 +37,7 @@ from keras.applications.imagenet_utils import decode_predictions
 # In[3]:
 
 
-#testing the data processing
+# testing the data processing
 pcd_path = "/storage/remote/atcremers62/kitti/kitti_data/training/velodyne/"
 label_path = "/storage/remote/atcremers62/kitti/kitti_data/training/label_2/"
 calib_path = "/storage/remote/atcremers62/kitti/kitti_data/training/calib/"
@@ -51,22 +50,23 @@ img_path = "/storage/remote/atcremers62/kitti/kitti_data/training/image_2/"
 # In[4]:
 
 
-#loading files and paths
+# loading files and paths
 BASE_DIR = os.path.dirname(os.path.abspath('__file__'))
 sys.path.append(BASE_DIR)
+
 
 def load_pc_from_bin(bin_path):
     """Load a bin file to a np array"""
     num_select = 2048
-    selected_points =[]
-    obj = np.fromfile(bin_path, dtype = np.float32).reshape(-1,4)
+    selected_points = []
+    obj = np.fromfile(bin_path, dtype=np.float32).reshape(-1, 4)
     pc = filter_camera_angle(obj)
     index = np.random.choice(pc.shape[0], num_select, replace=False)
     for i in range(len(index)):
         selected_points.append(pc[index[i]][0:3])
-    selected_points = np.array(selected_points).reshape(1,-1,3)# return N*3 array
+    selected_points = np.array(selected_points).reshape(
+        1, -1, 3)  # return N*3 array
     return selected_points
-
 
 
 def read_calib_file(calib_path):
@@ -105,26 +105,28 @@ def read_label_from_txt(label_path):
             label = label.split(" ")
             if (label[0] == "DontCare"):
                 continue
-            y_class = [int(label[0]=="Car"), int(label[0]=="Van"), int(label[0]=="Pedestrian")]
-            if label[0] == ("Car") or label[0]=="Van" or label[0]=="Pedestrian": #  or "Truck"
+            y_class = [int(label[0] == "Car"), int(
+                label[0] == "Van"), int(label[0] == "Pedestrian")]
+            if label[0] == ("Car") or label[0] == "Van" or label[0] == "Pedestrian":  # or "Truck"
                 y_labels = list(np.hstack((label[8:15], y_class)))
                 bounding_box.append(y_labels)
-                break # ? Why we break here?
+                break  # ? Why we break here?
 
     if bounding_box:
         data = np.array(bounding_box, dtype=np.float32)
         # class is 1x3 row here
-        return data[:, 3:6], data[:, :3], data[:, 6], data[:,7:] 
+        return data[:, 3:6], data[:, :3], data[:, 6], data[:, 7:]
 
     else:
         return None, None, None, None
+
 
 def read_labels(label_path, label_type, calib_path=None, is_velo_cam=False, proj_velo=None):
     """Read labels from txt file.
     Original Label value is shifted about 0.27m from object center.
     So need to revise the position of objects.
     """
-    if label_type == "txt": #TODO
+    if label_type == "txt":  # TODO
         places, size, rotates, y_class = read_label_from_txt(label_path)
         if places is None:
             return None, None, None, None
@@ -139,11 +141,12 @@ def read_labels(label_path, label_type, calib_path=None, is_velo_cam=False, proj
             places[:, 0] += 0.27
 
     data_combined = []
-    for p, r, s , cl in zip(places, rotates, size, y_class):
-        ps = np.hstack((cl[:],p[:],s[:]))
+    for p, r, s, cl in zip(places, rotates, size, y_class):
+        ps = np.hstack((cl[:], p[:], s[:]))
         data_combined.append(list(np.append(ps, r)))
-        
+
     return places, rotates, size, y_class
+
 
 def get_boxcorners(places, rotates, size):
     """Create 8 corners of bounding box from bottom center."""
@@ -178,29 +181,37 @@ def get_boxcorners(places, rotates, size):
         corners.append(a)
     return np.array(corners)
 
+
 def filter_camera_angle(places):
     """Filter camera angles for KiTTI Datasets"""
-    bool_in = np.logical_and((places[:, 1] < places[:, 0] - 0.27), (-places[:, 1] < places[:, 0] - 0.27))
+    bool_in = np.logical_and(
+        (places[:, 1] < places[:, 0] - 0.27), (-places[:, 1] < places[:, 0] - 0.27))
     # bool_in = np.logical_and((places[:, 1] < places[:, 0]), (-places[:, 1] < places[:, 0]))
     return places[bool_in]
+
 
 def center_to_sphere(places, size, resolution=0.50, min_value=np.array([0., -50., -4.5]), scale=4, x=(0, 90), y=(-50, 50), z=(-4.5, 5.5)):
     """Convert object label to Training label for objectness loss"""
     x_logical = np.logical_and((places[:, 0] < x[1]), (places[:, 0] >= x[0]))
     y_logical = np.logical_and((places[:, 1] < y[1]), (places[:, 1] >= y[0]))
     z_logical = np.logical_and((places[:, 2] < z[1]), (places[:, 2] >= z[0]))
-    xyz_logical = np.logical_and(x_logical, np.logical_and(y_logical, z_logical))
+    xyz_logical = np.logical_and(
+        x_logical, np.logical_and(y_logical, z_logical))
     center = places.copy()
     center[:, 2] = center[:, 2] + size[:, 0] / 2.
-    sphere_center = ((center[xyz_logical] - min_value) / (resolution * scale)).astype(np.int32)
+    sphere_center = ((center[xyz_logical] - min_value) /
+                     (resolution * scale)).astype(np.int32)
     return sphere_center
+
 
 def sphere_to_center(p_sphere, resolution=0.5, scale=4, min_value=np.array([0., -50., -4.5])):
     """from sphere center to label center"""
     center = p_sphere * (resolution*scale) + min_value
     return center
 
-#derived from yukitsuji/3D_CNN_tensorflow
+# derived from yukitsuji/3D_CNN_tensorflow
+
+
 def process(velodyne_path, label_path=None, calib_path=None, dataformat="bin", label_type="txt", is_velo_cam=False):
     p = []
     pc = None
@@ -209,8 +220,9 @@ def process(velodyne_path, label_path=None, calib_path=None, dataformat="bin", l
     rotates = None
     size = None
     proj_velo = None
-    
-    filenames_velo = [d for d in sorted(os.listdir(velodyne_path)) if d[0]!='.']
+
+    filenames_velo = [d for d in sorted(
+        os.listdir(velodyne_path)) if d[0] != '.']
     train_points = None
     train_labels = None
     train_classes = None
@@ -223,7 +235,8 @@ def process(velodyne_path, label_path=None, calib_path=None, dataformat="bin", l
         cur_points = load_pc_from_bin(velo_path)
         cur_calib = read_calib_file(cal_path)
         proj_velo = proj_to_velo(cur_calib)[:, :3]
-        places, rotates, size, classes= read_labels(lab_path, label_type, cal_path , is_velo_cam=is_velo_cam, proj_velo=proj_velo)
+        places, rotates, size, classes = read_labels(
+            lab_path, label_type, cal_path, is_velo_cam=is_velo_cam, proj_velo=proj_velo)
         corners = get_boxcorners(places, rotates, size)
         if places is None:
             continue
@@ -232,24 +245,25 @@ def process(velodyne_path, label_path=None, calib_path=None, dataformat="bin", l
             train_points = cur_points
             train_classes = classes
         else:
-            train_points = np.concatenate((train_points, cur_points), axis = 0)
-            train_labels = np.concatenate((train_labels, corners), axis = 0)
-            train_classes = np.concatenate((train_classes, classes), axis =0)
-            
+            train_points = np.concatenate((train_points, cur_points), axis=0)
+            train_labels = np.concatenate((train_labels, corners), axis=0)
+            train_classes = np.concatenate((train_classes, classes), axis=0)
+
     return train_points, train_labels, train_classes
 
 
 # In[5]:
 
 
-train_points, train_labels, train_classes = process(pcd_path, label_path, calib_path=calib_path, dataformat="bin", is_velo_cam=True)
+train_points, train_labels, train_classes = process(
+    pcd_path, label_path, calib_path=calib_path, dataformat="bin", is_velo_cam=True)
 
 #
 
 # In[6]:
 
 
-##print(train_classes.shape)
+# print(train_classes.shape)
 ##np.save('train_classes.npy', train_classes)
 ##np.save('train_points.npy', train_points)
 ##np.save('train_labels.npy', train_labels)
@@ -258,7 +272,7 @@ train_points, train_labels, train_classes = process(pcd_path, label_path, calib_
 # In[7]:
 
 
-#print(train_labels.reshape((7481,24)).shape)
+# print(train_labels.reshape((7481,24)).shape)
 
 
 # Generate image data and processing using Resnet-50 model:
@@ -267,10 +281,10 @@ train_points, train_labels, train_classes = process(pcd_path, label_path, calib_
 # In[ ]:
 
 
-#run this cell only if images need to be processed again
+# run this cell only if images need to be processed again
 #filenames_imgs = [img_path+d for d in sorted(os.listdir(img_path)) if d[0]!='.']
 #batch_images = None
-#for d in filenames_imgs:
+# for d in filenames_imgs:
 #    print(d)
 #    original = load_img(d, target_size=(224, 224))
 #    numpy_image = img_to_array(original)
@@ -284,14 +298,13 @@ train_points, train_labels, train_classes = process(pcd_path, label_path, calib_
 # In[3]:
 
 
-from keras.applications import resnet50
 resnet_model = resnet50.ResNet50(weights='imagenet')
 
 
 # In[15]:
 
 
-#run this cell only if images need to be processed again. Otherwise, use the saved file in next cell
+# run this cell only if images need to be processed again. Otherwise, use the saved file in next cell
 #processed_image = resnet50.preprocess_input(batch_images.copy())
 #np.save('processed_image.npy', processed_image)
 
@@ -299,20 +312,19 @@ resnet_model = resnet50.ResNet50(weights='imagenet')
 # In[ ]:
 
 
-#run this cell only if images need to be processed again by resnet. Otherwise, use the saved file in next cell
-from keras.backend.tensorflow_backend import set_session
+# run this cell only if images need to be processed again by resnet. Otherwise, use the saved file in next cell
 processed_images = np.load('processed_image.npy')
-#tf.compat.v1.diable_eager_execution()
+# tf.compat.v1.diable_eager_execution()
 #config = tf.ConfigProto()
 #config.gpu_options.allow_growth = True
 init = tf.global_variables_initializer()
 sess = tf.Session()
-#set_session(sess)
-#keras.backend.clear_session()
+# set_session(sess)
+# keras.backend.clear_session()
 sess.run(init)
 
-#extract final layer called flatten_1 with size 1 by 2048
-layer_name = 'avg_pool'#'flatten_1'
+# extract final layer called flatten_1 with size 1 by 2048
+layer_name = 'avg_pool'  # 'flatten_1'
 layer = resnet_model.get_layer(layer_name)
 
 #layer = resnet_model.layers[layer_num]
@@ -331,4 +343,3 @@ print("Shape of layer output:", intermediate_output.shape)
 
 intermediate_output = np.load('intermediate_output.npy')
 print(intermediate_output.shape)
-
